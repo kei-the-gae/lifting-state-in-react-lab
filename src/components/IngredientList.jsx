0dd786f440b86793f47d01d965c460c5ec2d33a9
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-08-15 14:57:51 -0700

    Add rendering of available ingredients array and add button to ingredient list

--- src/components/IngredientList.jsx
+++ src/components/IngredientList.jsx
@@ -1,5 +1,13 @@
+import { availableIngredients } from '../App.jsx';
+
 const IngredientList = (props) => {
-    return <ul>// map through props.ingredients</ul>;
+    return (
+        <ul>
+            {availableIngredients.map((ingredient, i) => (
+                <li key={i} style={{ backgroundColor: ingredient.color }}>{ingredient.name}<button>+</button></li>
+            ))}
+        </ul >
+    );
 };
 
 export default IngredientList;
